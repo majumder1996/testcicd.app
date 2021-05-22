71d1d40388e505c09162668aea09687620a457a1
majumder1996 <83267846+majumder1996@users.noreply.github.com>
2021-05-22 22:15:48 +0530

    changed server port to 3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 app.get('/message', function (req, res) {
      res.send('This is the message');
 });
